e1ca93dbcd9d29232ecf575847cc8c80bc2cd827
JennaMTucker <jenna.m.tucker@gmail.com>
2022-03-05 02:57:39 -0800

    after a million years, correct winston logging

--- startup/logger.js
+++ startup/logger.js
@@ -1,4 +1,5 @@
 const winston = require('winston')
+winston.remove(winston.transports.Console)
 require('winston-mongodb')
 require('express-async-errors')
 
@@ -6,30 +7,41 @@ const dbUrl = 'mongodb://localhost/my-vidly'
 
 // winston logger for error middleware
 
+const { format } = winston
+
+const winstonConsoleLogger = new winston.transports.Console()
+
+const createWinstonMongoDbLogger = (collection) => {
+	return new winston.transports.MongoDB({
+		db: dbUrl,
+		options: { useNewUrlParser: true, useUnifiedTopology: true },
+		collection: collection ? collection : 'log',
+		level: 'error'
+	})
+}
+
 const logger = winston.createLogger({
-	level: 'info',
-	format: winston.format.json(),
+	level: 'silly',
+	format: winston.format.combine(
+		winston.format.colorize({ all: true }),
+		winston.format.align(),
+		winston.format.printf((info) => `${info.level}: ${info.message}`)
+	),
+	exitOnError: true,
 	transports: [
-		new winston.transports.Console({ format: winston.format.simple() }),
+		winstonConsoleLogger,
 		new winston.transports.File({ filename: 'error.log', level: 'error' }),
-		new winston.transports.MongoDB({
-			db: dbUrl,
-			options: { useNewUrlParser: true, useUnifiedTopology: true },
-			collection: 'log-uncaughtException',
-			level: 'error'
-		})
+		createWinstonMongoDbLogger()
 	],
 	exceptionHandlers: [
-		new winston.transports.Console({ format: winston.format.simple() }),
+		winstonConsoleLogger,
 		new winston.transports.File({ filename: 'uncaughtExceptions.log' }),
-		new winston.transports.MongoDB({
-			db: dbUrl,
-			options: {
-				useNewUrlParser: true,
-				useUnifiedTopology: true
-			},
-			level: 'error'
-		})
+		createWinstonMongoDbLogger('log-uncaughtExceptions')
+	],
+	rejectionHandlers: [
+		winstonConsoleLogger,
+		new winston.transports.File({ filename: 'unhandledRejections.log' }),
+		createWinstonMongoDbLogger('log-unhandledRejections')
 	]
 })
 

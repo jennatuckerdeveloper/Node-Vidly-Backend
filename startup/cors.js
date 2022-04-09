b3a69087ac9f5e83487852cf0f3024e7145cb7e8
Dev Workspace <DevWork@Jenna-M-Tuckers-MacBook-2.local>
2022-04-09 14:33:03 -0700

    allowing cors so I can use remote heroku with thunder client on frontend

--- startup/cors.js
+++ startup/cors.js
@@ -8,10 +8,7 @@ module.exports = function (app) {
 	]
 	const corsOptions = {
 		origin: function (origin, callback) {
-			if (
-				whitelist.indexOf(origin) !== -1 ||
-				(!origin && process.env.NODE_ENV !== 'production')
-			) {
+			if (whitelist.indexOf(origin) !== -1 || !origin) {
 				callback(null, true)
 			} else {
 				callback(new Error('Not allowed by CORS'))
